bc6f554a3c439badd332a26589a6a524d116ab19
915-Patrulescu-Sandrino <prie2985@scs.ubbcluj.ro>
2021-03-22 18:28:09 +0200

    1st commit

---
+++ ADT_FixedCapBiMap_Lab_2/cmake-build-debug/CMakeFiles/LAB2_FixedCapBiMap.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB2_FixedCapBiMap.dir/App.cpp.o"
+  "CMakeFiles/LAB2_FixedCapBiMap.dir/ExtendedTest.cpp.o"
+  "CMakeFiles/LAB2_FixedCapBiMap.dir/FixedCapBiMap.cpp.o"
+  "CMakeFiles/LAB2_FixedCapBiMap.dir/FixedCapBiMapIterator.cpp.o"
+  "CMakeFiles/LAB2_FixedCapBiMap.dir/ShortTest.cpp.o"
+  "LAB2_FixedCapBiMap"
+  "LAB2_FixedCapBiMap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAB2_FixedCapBiMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
